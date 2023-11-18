511579513e5c6f9332b249fe53f5a9ae615bcb2b
Balázs Varga <34116704+MrN00b1101@users.noreply.github.com>
2023-11-18 20:37:04 +0100

    Update validate.sh

--- validate.sh
+++ validate.sh
@@ -1,1 +1,10 @@
-echo "Műkdödik"
+#!/bin/bash
+    # Check if specific files exist
+    if [ -f "1.txt" ] && [ -f "o2.txt" ]; then
+        echo "Validation successful. All required files exist."
+        exit 0  # Exit with success status
+    else
+        echo "Error: Some or all required files are missing."
+        exit 1  # Exit with failure status
+    fi
+
