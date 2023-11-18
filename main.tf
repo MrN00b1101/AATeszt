af381e6267b0a82d272222f9bce6d705093e6fc5
Balázs Varga <34116704+MrN00b1101@users.noreply.github.com>
2023-11-18 19:44:51 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ variable "file_name" {
   type        = string
   default = "teszt001"
 }
+resource "local_file" "first_file" {
+  content = "This is my text"
+  filename = "../first_file.txt"
+}
 resource "local_file" "myfile" {
   content = "This is my text"
   filename = format("%s/%s%s","..*",var.file_name,".txt")
