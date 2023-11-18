516659ad4df5374ceacd4bf70406dff7062a63f7
Balázs Varga <34116704+MrN00b1101@users.noreply.github.com>
2023-11-18 20:43:21 +0100

    Update validate.sh

--- validate.sh
+++ validate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-    # Check if specific files exist
-    if [ -f "../1.txt" ] && [ -f "../2.txt" ]; then
+    # Check if specific files exist teszt001 first_file.txt
+    if [ -f "../teszt001.txt" ] && [ -f "../first_file.txt" ] && [ -f "../0.txt" ] && [ -f "../1.txt" ] && [ -f "../2.txt" ] && [ -f "../3.txt" ] then
         echo "Validation successful. All required files exist."
         exit 0  # Exit with success status
     else
