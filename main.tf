d624e73a818288db20203a9e95727d7d5543d3a4
Balázs Varga <34116704+MrN00b1101@users.noreply.github.com>
2023-11-18 19:53:01 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,9 +16,8 @@ resource "local_file" "variable_file" {
 }
 resource "local_file" "count_file" {
   count = var.file_count
-  tags ={
-    content = format("/%s%s","iteration: ",var.file_count)
-    filename = format("%s/%s%s","../",var.file_count,".txt")
-  }
+  content ={format("/%s%s","iteration: ",var.file_count)}
+  filename =  {format("%s/%s%s","../",var.file_count,".txt")}
+  
   
 }
