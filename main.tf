5485393746a916e9613dd5f6ca8196d94e0352d1
Balázs Varga <34116704+MrN00b1101@users.noreply.github.com>
2023-11-18 19:49:22 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,15 @@ resource "local_file" "first_file" {
   content = "This is my text"
   filename = "../first_file.txt"
 }
-resource "local_file" "myfile" {
+resource "local_file" "variable_file" {
   content = "This is my text"
   filename = format("%s/%s%s","..*",var.file_name,".txt")
+}
+resource "local_file" "count_file" {
+  count = var.file_count
+  tags ={
+    content = "{var.file_name}. iteration."
+    filename = "../${var.file_name}.txt")
+  }
+  
 }
